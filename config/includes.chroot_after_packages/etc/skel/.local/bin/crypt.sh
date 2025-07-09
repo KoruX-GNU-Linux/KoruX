cee10ddb911ca0218d484cd832daa2684d5533fa
Kirill Rekhov <krekhov.dev@gmail.com>
2025-07-10 00:33:53 +0300

    add crypt.sh script to ~/.local/bin

---
+++ config/includes.chroot_after_packages/etc/skel/.local/bin/crypt.sh
@@ -0,0 +1,71 @@
+#!/bin/sh
+#
+# Encrypt/decrypt regular file with sensitive information (using a password).
+#
+
+usage()
+{
+if [ $1 -eq 1 ]; then
+	>&2 echo "Try '$(basename $0) --help' for more information."
+	exit 1
+else
+cat << EOF
+Usage: $(basename $0) [option]
+Use the AES-256 (Advanced Encryption Standard) cipher for symmetric file
+encryption and decryption.
+
+  [option]
+  -f, --file </path/to/file>     specify file (regular or openssl)
+  -h, --help                     show this help and exit
+
+EOF
+
+exit 0
+fi
+}
+
+case "$1" in
+'-f'|'--file')
+	if [ ! -f "$2" ] || [ -z "$2" ]; then
+		>&2 echo "\$2 incorrect."
+		usage 1
+	fi
+
+	if [ "$USER" != "$(stat -c %U $2)" ]; then
+		>&2 echo "'$USER' you have no perms."
+		exit 1
+	fi
+
+	randf=".$(tr -dc 'a-z0-9' < /dev/urandom | head -c 8)"
+
+	while true; do
+		if file "$2" | grep -E "openssl|salted"; then
+			# decrypt file
+			if ! openssl enc -d -aes256 -pbkdf2 -in "$2" 2>/dev/null; then
+				>&2 echo "Something went wrong, try again."
+				exit 1
+			fi
+		else
+			# encrypt new file
+			if ! openssl enc -e -aes256 -pbkdf2 -in "$2" -out "$randf"; then
+				continue
+			fi
+
+			shred -uzn 8 "$2" && echo "shred $2"
+			chmod 600 "$randf"
+			realpath "$randf"
+			break
+		fi
+
+		break
+	done
+	;;
+
+'-h'|'--help')
+	usage 0
+	;;
+
+*)
+	usage 1
+	;;
+esac
